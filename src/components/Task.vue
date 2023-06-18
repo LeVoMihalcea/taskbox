f590a009799fc38a3209ff2ae3420267cadb5d4c
Leo Mihalcea <leonard.mihalcea@cloudflight.io>
2023-06-18 15:08:06 +0300

    feat: Implemented Task Component and added a Story for it

---
+++ src/components/Task.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="list-item">
+        <label for="title" :aria-label="task.title">
+            <input type="text" readonly :value="task.title" id="title" name="title" />
+        </label>
+    </div>
+</template>
+  
+<script setup lang="ts">
+import {ref} from 'vue';
+
+interface Task {
+    id: String,
+    title: String,
+    state: String
+}
+
+const props = defineProps({
+    task: {
+        type: Object as () => Task,
+        required: true,
+        default: () => ({}),
+    }
+});
+
+const task = ref(props.task);
+
+console.log(props.task);
+</script>
