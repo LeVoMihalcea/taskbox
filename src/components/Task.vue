0eab267b951758c782ab140776225aaead574ff6
Leo Mihalcea <leonard.mihalcea@cloudflight.io>
2023-06-18 15:33:49 +0300

    feat: Finished Task Component implementation

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,19 +1,31 @@
 <template>
-    <div class="list-item">
-        <label for="title" :aria-label="task.title">
-            <input type="text" readonly :value="task.title" id="title" name="title" />
+    <div class="list-item" :class="stateClass">
+        <label :for="'checked' + task.id" :aria-label="'archiveTask-' + task.id" class="checkbox">
+            <input type="checkbox" :checked="isChecked" disabled :name="'checked' + task.id"
+                :id="'archiveTask-' + task.id" />
+            <span class="checkbox-custom" @click="archiveTask" />
         </label>
+        <label :for="'title-' + task.id" :aria-label="task.title" class="title">
+            <input type="text" readonly :value="task.title" :id="'title-' + task.id" name="title"
+                placeholder="Input title" />
+        </label>
+        <button v-if="!isChecked" class="pin-button" @click="pinTask" :id="'pinTask-' + task.id"
+            :aria-label="'pinTask-' + task.id">
+            <span class="icon-star" />
+        </button>
     </div>
 </template>
   
 <script setup lang="ts">
-import {ref} from 'vue';
+import { ref, computed } from 'vue';
 
 interface Task {
     id: String,
     title: String,
     state: String
 }
+
+const emit = defineEmits(['archive-task', 'pin-task'])
 
 const props = defineProps({
     task: {
@@ -25,5 +37,32 @@ const props = defineProps({
 
 const task = ref(props.task);
 
+const isChecked = computed(() => {
+    return task.value.state === 'TASK_ARCHIVED';
+})
+
+const stateClass = computed(() => {
+    return task.value.state;
+})
+
+
+function archiveTask() {
+    emit('archive-task', props.task.id);
+}
+function pinTask() {
+    emit('pin-task', props.task.id);
+}
+
 console.log(props.task);
 </script>
+
+<style scoped>
+.pinned {
+    background: yellow;
+}
+
+.archived {
+    background: gray;
+    text-decoration: line-through;
+}
+</style>
